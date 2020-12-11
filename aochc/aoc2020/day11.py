8ca1c1c48ea872129c28687310cdbb9dfe566293
thoenis <sthoeni@kof.ethz.ch>
2020-12-11 22:36:43 +0100

    convoluted solution for day11part1
    get it? ;P

--- aochc/aoc2020/day11.py
+++ aochc/aoc2020/day11.py
@@ -1,40 +1,36 @@
 import numpy as np
+from scipy import signal
+
 import copy
 
 def prepare(input):
     return np.array([[let for let in line] for line in input.splitlines()])
 
-def get_n_free_neighbours(array, row, col):
-    n = 0
-    for r in range(row - 1, row + 2): # whyyyyyy?
-        for c in range(col - 1, col + 2):
-            if r >= 0 and r < len(array) and c >= 0 and c < len(array[0]) and not (r == row and c == col):
-                n += array[r][c] in ['L', '.']
-            elif not (r == row and c == col):
-                n += 1
+def purdy_purnt(waiting_area):
+    for l in waiting_area:
+        print(''.join(l))
+    print('')
 
-    return n
+# Aw hell, I learned about them so why not?
+def step_a(array):
+    array = copy.deepcopy(array)
+    filt = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
-def step(array):
-    next_step = copy.deepcopy(array)
-    for r in range(0, len(next_step)):
-        for c in range(0, len(next_step[0])):
-            if not next_step[r][c] == '.':
-                n_free = get_n_free_neighbours(array, r, c)
-                if n_free == 8:
-                    next_step[r][c] = '#'
-                elif n_free <= 4:
-                    next_step[r][c] = 'L'
-    return next_step 
+    while True:
+        occupied = array == '#'
+        filtered = signal.convolve2d(occupied, filt, 'same')
+        array[(array == 'L') & (filtered == 0)] = '#'
+        array[(array == '#') & (filtered >= 4)] = 'L'
+        yield copy.deepcopy(array)
 
-def part_a(waiting_area):
-    prev = copy.deepcopy(waiting_area)
-    waiting_area = step(waiting_area)
-    while not np.all(prev == waiting_area):
-        prev = copy.deepcopy(waiting_area)
-        waiting_area = step(waiting_area)
-
-    return np.sum(waiting_area == '#')
+def part_a(waiting_area, prnt = False):
+    target = copy.deepcopy(waiting_area)
+    for wa in step_a(waiting_area):
+        if prnt:
+            purdy_purnt(wa)
+        if np.all(wa == target):
+            return np.sum(wa == '#')
+        target = wa
 
 def get_n_free_neighbours_b(array, row, col):
     n = 8
@@ -65,7 +61,7 @@ def step_b(array):
                     next_step[r][c] = 'L'
     return next_step
 
-def part_b(waiting_area):
+def part_bb(waiting_area):
     prev = copy.deepcopy(waiting_area)
     waiting_area = step_b(waiting_area)
     while not np.all(prev == waiting_area):
@@ -90,7 +86,8 @@ L.LLLLL.LL
     #print(get_n_free_neighbours(prepare(example1), 1, 1))
     #print(step(prepare(example1)))
     #print(step(step(prepare(example1))))
-    #print(part_a(prepare(example1)))
+    print(part_a(prepare(example1), prnt = True))
+
 
     example2 = prepare('''.......#.
 ...#.....
@@ -117,7 +114,7 @@ L.LLLLL.LL
 .##.##.
 ''')
 
-    print(get_n_free_neighbours_b(example2, 4, 3))
-    print(get_n_free_neighbours_b(example3, 1, 1))
-    print(get_n_free_neighbours_b(example4, 3, 3))
-    print(part_b(prepare(example1)))
+    #print(get_n_free_neighbours_b(example2, 4, 3))
+    #print(get_n_free_neighbours_b(example3, 1, 1))
+    #print(get_n_free_neighbours_b(example4, 3, 3))
+    #print(part_b(prepare(example1)))
